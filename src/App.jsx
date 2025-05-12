0f248ab68fece58c2c21b52b1d6a23c6aa684bdc
abhishekdav003 <abhishekdav003@gmail.com>
2025-05-12 16:15:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import About from './pages/About';
+import Product from './pages/Product';
+import Sustainability from './pages/Sustainability';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/sustainability" element={<Sustainability />} />
+      <Route path="/contact" element={ <Contact/>} />
+
+      
+    </Routes>
+  )
+}
+
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+
+function About() {
+  return (
+    <>
+      <Header />
+      About
+      <Footer />
+      
+    </>
+  )
+}
+
+export default About
